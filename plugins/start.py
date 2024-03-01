48a5d6158c84c0965a06077ec5a91fa4a01ebd53
ujjwal3155 <153728856+ujjwal3155@users.noreply.github.com>
2024-03-01 21:45:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -86,8 +86,8 @@ async def start_command(client: Client, message: Message):
             InlineKeyboardButton("ʜᴇʟᴘ​", callback_data="help")
         ],
         [
-            InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/Anime_Mayhem"),
-            InlineKeyboardButton("ᴏɴɢᴏɪɴɢ", url="https://t.me/Ongoing_Anime_Mayhem")
+            InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/Otaku_Vision_M"),
+            InlineKeyboardButton("ᴏɴɢᴏɪɴɢ", url="https://t.me/otaku_vision_chat")
         ],
         [
             InlineKeyboardButton("💥ᴄʟᴏꜱᴇ💥", callback_data="close")
